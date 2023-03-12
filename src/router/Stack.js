81c705c281427f05faeb00bdfc5b3d1c96abd5a3
kienmarco2408 <kienmarco999@gmail.com>
2023-03-13 03:11:17 +0700

    update

--- src/router/Stack.js
+++ src/router/Stack.js
@@ -1,15 +1,17 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import SignIn from '../screen/SignIn';
+import { View, Text } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import SignIn from "../screen/SignIn";
+import LogIn from "../screen/LogIn";
 const Stacks = createNativeStackNavigator();
 const RootStack = () => {
   return (
     <NavigationContainer>
       <Stacks.Navigator screenOptions={{ headerShown: false }}>
         <Stacks.Screen name="SignIn" component={SignIn} />
+        <Stacks.Screen name="Login" component={LogIn} />
       </Stacks.Navigator>
     </NavigationContainer>
   );
